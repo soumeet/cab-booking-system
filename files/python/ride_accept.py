e432064939749aa01ddba43f3c2530d64acd7851
soumeet <soumeet.basak@gmail.com>
2016-09-04 03:08:34 +0530

    commit_04092016
    html, js updated.

--- files/python/ride_accept.py
+++ files/python/ride_accept.py
@@ -23,6 +23,12 @@ for r in cur:
     cid=r[0]
 
 if sts==-1:
+    sql="UPDATE DRIVER SET STATUS = 'ENROUTE' WHERE DRIVER_ID = %s" % (did)
+    cur.execute(sql)
+    cur.execute('commit')
+    sql="UPDATE CUSTOMER SET STATUS = 'ENROUTE' WHERE CUSTOMER_ID = %s" % (cid)
+    cur.execute(sql)
+    cur.execute('commit')
     sql="UPDATE RIDE SET UPTIME = '%s', STATUS = 'ACCEPTED' WHERE DRIVER_ID = %s AND CUSTOMER_ID = %s AND STATUS = 'REQUESTED'" % (now, did, cid)
     cur.execute(sql)
     cur.execute('COMMIT')
